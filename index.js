80003a63331d661463d391ca75a2a4ca58cc4987
David <david.owuor@student.moringaschool.com>
2022-11-27 13:00:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+
+    let decision = record.find((decision)=>{
+        
+        if (decision.result==='W'){
+            return true;
+        }
+    })
+        if (decision){
+            return decision.year
+        }
+}
